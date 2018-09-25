336125a2e75490ce20155977a7e829d1bf773b2d
Győri Sándor <alexaegis@INF.ELTE.HU>
2018-09-25 15:39:34 +0200

    the promise way

--- lesson03/task4_readline.js
+++ lesson03/task4_readline.js
@@ -9,7 +9,7 @@ rl.question('Your name: ', a => {
 	console.log(`Hi, ${a}!`);
 	rl.close();
 });*/
-
+/*
 rl.question('operand 1: ', o1 => {
 	rl.question('operator: ', o => {
 		rl.question('operand 2: ', o2 => {
@@ -17,4 +17,28 @@ rl.question('operand 1: ', o1 => {
 			rl.close();
 		});
 	});
-});
+}); */
+
+const question = q => {
+	return new Promise(res => {
+		rl.question(q, o => {
+			res(o);
+		});
+	});
+};
+
+let op1, op, op2;
+question('operand 1: ')
+	.then(o1 => {
+		op1 = o1;
+		return question('operator: ');
+	})
+	.then(o => {
+		op = o;
+		return question('operand 2: ');
+	})
+	.then(o2 => {
+		op2 = o2;
+		console.log(eval(op1 + op + op2));
+		rl.close();
+	});
