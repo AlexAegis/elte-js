896c2f6d2b6df48643bb11ff670077ba9e7e0950
Győri Sándor <alexaegis@gmail.com>
2019-01-13 18:18:18 +0100

    added authorization

--- src/main/java/hu/elte/assignment/controller/CaesarController.java
+++ src/main/java/hu/elte/assignment/controller/CaesarController.java
@@ -3,6 +3,7 @@ package hu.elte.assignment.controller;
 import hu.elte.assignment.api.Response;
 import lombok.experimental.FieldDefaults;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +16,7 @@ import static lombok.AccessLevel.PRIVATE;
 @FieldDefaults(level = PRIVATE, makeFinal = true)
 public class CaesarController {
 
+	@PreAuthorize("hasAuthority('USER')")
 	@GetMapping("/{seed}")
 	public ResponseEntity<Response<Integer>> getKey(@PathVariable("seed") String seed) {
 		return ResponseEntity.ok(Response.<Integer>builder().data(seed.chars().sum()).build());
